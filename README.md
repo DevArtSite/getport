679a64603c46448e89e8063c3487ed3550adb107
MNLaugh <contact@mnlaugh.com>
2021-04-19 19:22:30 +0200

    Update & test

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-<div align="center">
-  <a href="https://devartsite.com/opensource" target="_blank">
-    <img src="https://devartsite.com/assets/front/v1/img/logo-longtext.png" width="250px">
-    <br>
-    <sub><h5>Devartsite helps make open source sustainable.</h5></sub>
-  </a>
-</div>
-
-# getport [![Build Status](https://travis-ci.org/devartsite/getport.svg?branch=master)](https://travis-ci.org/devartsite/getport)
+# getport [![Build Status](https://travis-ci.com/devartsite/getport.svg?branch=master)](https://travis-ci.com/devartsite/getport)
 
 > Get an available [TCP port](https://en.wikipedia.org/wiki/Port_(computer_networking))
 
@@ -15,9 +7,7 @@
 ```ts
 import { getPort } from "https://deno.land/x/getport/mod.ts";
 
-const port = await getPort({}).catch((error: Error) => {
-  throw error;
-});
+const port = await getPort({}).catch(console.error);
 
 console.log(port); //=> fall back to a random port
 ```
@@ -27,9 +17,7 @@ Pass in a preferred port:
 ```ts
 import { getPort } from "https://deno.land/x/getport/mod.ts";
 
-const port = await getPort({ port: 3000 }).catch((error: Error) => {
-  throw error;
-});
+const port = await getPort({ port: 3000 }).catch(console.error);
 
 console.log(port); //=> Will use 3000 if available, otherwise fall back to a random port
 ```
@@ -39,9 +27,7 @@ Pass in an array of preferred ports:
 ```ts
 import { getPort } from "https://deno.land/x/getport/mod.ts";
 
-const port = await getPort({ port: [3000, 3001, 3002] }).catch((error: Error) => {
-  throw error;
-});
+const port = await getPort({ port: [3000, 3001, 3002] }).catch(console.error);
 
 console.log(port); //=> Will use any element in the preferred ports array if available, otherwise fall back to a random port
 ```
@@ -51,9 +37,7 @@ Use the `makeRange()` helper in case you need a port in a certain range:
 ```ts
 import { getPort, makeRange } from "https://deno.land/x/getport/mod.ts";
 
-const port = await getPort({ port: makeRange(3000, 3100) }).catch((error: Error) => {
-  throw error;
-});
+const port = await getPort({ port: makeRange(3000, 3100) }).catch(console.error);
 
 console.log(port); //=> Will use any port from 3000 to 3100, otherwise fall back to a random port
 ```
@@ -97,9 +81,3 @@ First port of the range. Must be in the range `1024`...`65535`.
 Type: `number`
 
 Last port of the range. Must be in the range `1024`...`65535` and must be greater than `from`.
-
-<style type="text/css">
-.markdown-body a:hover, .markdown-body a:focus, .markdown-body a:active {
-  text-decoration: none;
-}
-</style>
--- mod.ts
+++ mod.ts
@@ -1,59 +1,60 @@
 const min = 1024;
 const max = 65535;
-const getAvailablePort = (options: Deno.ListenOptions): Promise<number> => new Promise((resolve, reject) => {
-  try {
-    const server = Deno.listen(options);
-    const { port } = options;
-    server.close();
-    resolve(port);
-  } catch (e) {
-  	reject(e);
-  }
-})
 
-function makeRange(from: number, to: number): Iterable<number> {
-	if (!Number.isInteger(from) || !Number.isInteger(to)) throw new TypeError('`from` and `to` must be integer numbers');
-	if (from < min || from > max) throw new RangeError('`from` must be between 1024 and 65535');
-	if (to < min || to > max) throw new RangeError('`to` must be between 1024 and 65536');
-	if (to < from) throw new RangeError('`to` must be greater than or equal to `from`');
+export type checkedPort = {
+	valid: boolean;
+	port: number
+}
+function checkPort(options: Deno.ListenOptions): checkedPort {
+	const { port } = options;
+	try {
+		const server = Deno.listen(options);
+		server.close();
+		return { valid: true, port: port };
+	} catch (e) {
+		if (e.name !== 'AddrInUse') throw e;
+		else return { valid: false, port: port};
+	}
+}
+
+function makeRange(from: number, to: number): number[] {
+	if (!(from > min || from < max)) throw new RangeError('`from` must be between 1024 and 65535');
+	if (!(to > min || to < max)) throw new RangeError('`to` must be between 1024 and 65536');
+	if (!(to > from)) throw new RangeError('`to` must be greater than or equal to `from`');
 	var ports = [];
 	for (let port = from; port <= to; port++) ports.push(port);
 	return ports;
-};
-
-async function randomPort(options: any): Promise<any> {
-  const port = Math.ceil(Math.random() * (max - min) + min);
-  return await getAvailablePort({...options, port}).catch((error) => {
-  	handleError(error)
-  	return randomPort(options).catch(handleError);
-  }).catch(handleError);
-};
-
-function handleError (error: Error) {
-	if (error.name !== 'AddrInUse') throw error;
 }
 
-async function getPort (options: any) {
-	let ports: Iterable<number>;
-	let listenOptions: Deno.ListenOptions = {
-		hostname: options.hostname || '0.0.0.0',
-		port: (typeof options.port === 'number') ? options.port : 0
+function randomPort(hostname?: string): number {
+  const port = Math.ceil(Math.random() * ((max-1) - min+1) + min+1);
+  const result: checkedPort = checkPort({ hostname, port});
+  if (result.valid) return result.port;
+  else return randomPort(hostname);
+}
+
+export type Port = number | number[]
+function getPort (port?: Port, hostname?: string): number {
+	const listenOptions: Deno.ListenOptions = {
+		hostname: hostname || '0.0.0.0',
+		port: (port && !Array.isArray(port)) ? port : 0
 	}
 
-	if (!options.port || options.port === 0) return await randomPort(listenOptions).catch(handleError)
-	else if (typeof options.port === 'number' && options.port !== 0) {
-		return await getAvailablePort(listenOptions).catch(async (error) => {
-			handleError(error)
-			return await randomPort(listenOptions).catch(handleError);
-		});
-	} else if (typeof options.port === 'object') {
-		ports = options.port;
-	    for (const port of ports) {
-	    	const result: any = await getAvailablePort({...listenOptions, port}).catch(handleError);
-	    	if (!!result) return result
-	    }
-	} else throw new Error('No available ports found');
-};
+	if (!port || Array.isArray(port)) {
+		const ports: number[] = (Array.isArray(port))? port : makeRange(min+1, max-1);
+		for (const port of ports) {
+			const result: checkedPort = checkPort({...listenOptions, port});
+			if (result.valid) return result.port;
+		}
+		throw new Error(`No ports available in the range of ${ports[0]} to ${ports[ports.length-1]}.`)
+	} else {
+		const result: checkedPort = checkPort(listenOptions);
+		if (!result.valid) {
+			const range = makeRange(result.port + 1, max-1);
+			return getPort(range);
+		} else return result.port;
+	}
+}
 
-export { getPort, randomPort, makeRange }
+export { min, max, getPort, randomPort, makeRange }
 export default getPort
