687497e3a3fbceee4bfc288e73966c61244eb2f6
MNLaugh <contact@mnlaugh.com>
2021-05-27 00:51:45 +0200

    minor update

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ m="minor update"
 # global push in main
 git add .
 git commit -m "$m"
-git push origin master --force
+git push origin master
 
 # create new tag if an argument is set
 if [[ $1 ]]; then
@@ -13,5 +13,5 @@ if [[ $1 ]]; then
   git add .
   git commit -m "$m"
   git tag -a "$a" -m "Version $v"
-  git push origin "$a"
+  git push origin "$a" --force
 fi
